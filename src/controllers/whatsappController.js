3b501035d2cdd1af864b838c2a5503dffb7046fc
Ing Oscar Lobo <oscarescalando@gmail.com>
2024-09-15 21:44:35 -0400

    fix

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -75,7 +75,9 @@ exports.converterImage = async (req, res) => {
   }
   
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: "networkidle0" });
     const imageBuffer = await page.screenshot({
